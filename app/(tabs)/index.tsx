e6524ec6e1477c8b96d8c93545a34b772a8dba70
Jefferson Chen <me@chenjefferson.com>
2023-11-29 23:01:25 -0800

    add image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet } from 'react-native';
+import { Button, StyleSheet } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
 export default function TabOneScreen() {
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
+      <Button title="Open Photo Picker" onPress={pickImage} />
     </View>
   );
 }
